6bb625887e93b861e9c31c30aa973e8d5dc6ab80
SteffanvanderWerf02 <steffan.van.der.werf@student.nhlstenden.com>
2022-08-31 14:50:42 +0200

    bug fix opdracht c

--- Exercise1/Exercise1c/script.js
+++ Exercise1/Exercise1c/script.js
@@ -2,5 +2,5 @@ const speed = 300000000
 
 function calcJoule(weight) {
     //  E = m * c2
-    document.getElementById("calc2").innerText = weight * Math.pow(speed,2)
+    document.getElementById("jule").innerText = weight * Math.pow(speed,2)
 }
