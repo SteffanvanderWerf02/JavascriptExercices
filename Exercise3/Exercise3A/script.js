4eecabcd322f672478b182c80cc3b20bae1936a7
SteffanvanderWerf02 <steffan.van.der.werf@student.nhlstenden.com>
2022-09-05 11:46:11 +0200

    cleaned up code

--- Exercise3/Exercise3A/script.js
+++ Exercise3/Exercise3A/script.js
@@ -1,9 +1,8 @@
 let cookie = new LuckyCookie();
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     document.getElementById("quote").textContent = cookie.GiveMeAnNiceQuote();
-// })
 document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("quote").textContent = cookie.GiveMeAnNiceQuote();
+    //on click
     document.getElementById("nowPlease").addEventListener("click", function () {
         document.getElementById("quote").textContent = cookie.GiveMeAnNiceQuote();
     })
